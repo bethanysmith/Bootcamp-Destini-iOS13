f928cec9630d28887aae09d82a68177c25586581
Bethany Smith <bethanys1609@gmail.com>
2024-12-02 16:45:01 +1100

    Step 5: Create StoryBrain model to handle story capability

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,25 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    
+    var storyNumber = 0
+    
+    let stories: [Story] = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    mutating func nextStory(userChoice: String?) {
+        if userChoice == stories[0].choice1 {
+            storyNumber = 1
+        } else if userChoice == stories[0].choice2 {
+            storyNumber = 2
+        }
+    }
+    
+    var currentStory: Story {
+        return stories[storyNumber]
+    }
+}

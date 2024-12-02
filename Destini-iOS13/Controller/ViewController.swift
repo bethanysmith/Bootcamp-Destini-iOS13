101ac725c042415e456244efa411989ec3128c87
Bethany Smith <bethanys1609@gmail.com>
2024-12-02 16:36:30 +1100

    Step 1 - 4: Create Story struct and connect labels and buttons to functionality

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories: [Story] = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    var storyNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        storyNumber = 0
+        updateUI()
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.titleLabel?.text == stories[0].choice1 {
+            storyNumber = 1
+        } else if sender.titleLabel?.text == stories[0].choice2 {
+            storyNumber = 2
+        }
+        updateUI()
+    }
+    
+    private func updateUI() {
+        storyLabel.text = stories[storyNumber].title
+        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
     }
 
-
 }
-
